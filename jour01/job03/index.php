2f6ed0e37239b3862338ca879cf5d6410b717bd0
albertini <keany.albertini@laplateforme.io>
2021-03-11 13:41:05 +0100

    fin du job03 message dans la console

---
+++ jour01/job03/index.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<script src="script.js" type="text/javascript">
+
+	
+	</script>
+	<title> debut js</title>
+</head>
+<body>
+
+</body>
+</html>
