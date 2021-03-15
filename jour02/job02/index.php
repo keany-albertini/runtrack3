15416bcb22758c773c2bf1ea24dc17af6cf41bc3
albertini <keany.albertini@laplateforme.io>
2021-03-15 13:34:19 +0100

    ajout du job02

---
+++ jour02/job02/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>JOB 02</title>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+    <button id="button" onclick="showhide(button)"> Click </button>
+    <div id="texte" style="display: none">L'important n'est pas la chute, mais l'atterrissage.</div>
+</body>
+</html>
---
+++ jour02/job02/script.js
@@ -0,0 +1,7 @@
+function showhide(id){
+  if(document.getElementById("texte").style.display == "none"){
+    document.getElementById("texte").style.display = "block";
+  } else {
+    document.getElementById("texte").style.display = "none";
+  }
+}
